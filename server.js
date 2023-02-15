e3fc88bd25630a6c24f73af12c92c812ee332814
Mike Fomin <mikefomin@Mikes-MacBook-Pro.local>
2023-02-14 23:32:15 -0800

    changed up some files and added content to server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env || 8000;
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname + '/app/public')));
+
+require("./app/routing/api-routes.js")(app);
+
+require("./app/routing/html-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT)
+});
